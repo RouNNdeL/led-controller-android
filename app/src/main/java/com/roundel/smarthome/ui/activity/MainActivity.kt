074a02e33a86b874178749494280d0745fe7b71f
RouNdeL <krzys.zdulski@gmail.com>
2018-08-08 23:17:14 +0200

    Add a simple demo for RouNNdeL/esp8266-leds

--- app/src/main/java/com/roundel/smarthome/ui/activity/MainActivity.kt
+++ app/src/main/java/com/roundel/smarthome/ui/activity/MainActivity.kt
@@ -2,14 +2,21 @@ package com.roundel.smarthome.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.roundel.smarthome.R
+import com.roundel.smarthome.bind
+import com.roundel.smarthome.ui.view.LightOnOffButton
+import okhttp3.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    val lightButton: LightOnOffButton by bind(R.id.device_state_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,25 @@ class MainActivity : AppCompatActivity() {
         DrawerBuilder().withActivity(this).withAccountHeader(accountHeader).addDrawerItems(
                 PrimaryDrawerItem().withIdentifier(1).withName("Devices").withIcon(R.drawable.ic_lightbulb)
         ).build()
+
+        lightButton.onStatusChangedListener = object : LightOnOffButton.OnStatusChangedListener {
+            override fun onStatusChanged(status: Boolean) {
+
+                //TODO: Temporary demo
+                val client = OkHttpClient()
+
+
+                val body = RequestBody.create(MediaType.parse("text/plain; charset=utf-8"), "ff${if (status) "01" else "00"}*")
+                val request = Request.Builder()
+                        .url("http://192.168.1.11/globals")
+                        .put(body)
+                        .build()
+                Thread(Runnable {
+                    val response = client.newCall(request).execute()
+                    Log.d("TAG", response.message())
+                }).start()
+
+            }
+        }
     }
 }
